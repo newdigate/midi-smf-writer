69653518993bb6c88260cfdca8868840df459e16
newdigate <nicnewdigate@gmail.com>
2024-10-05 11:58:38 +0100

    printf

--- src/smfwriter.cpp
+++ src/smfwriter.cpp
@@ -281,7 +281,8 @@ int SmfWriter::flushWithErrorHandling() {
 }
 
 void SmfWriter::flush() {
-  Serial.printf("Bytes written: %i\n", _bytesWritten);
+  Serial.print("Bytes written: ");
+  Serial.println( _bytesWritten);
   int flushWithErrors = flushWithErrorHandling();
   if (flushWithErrors != 0) {
     _error = flushWithErrors;
