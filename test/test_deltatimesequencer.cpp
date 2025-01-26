04bfa8167d8799d8cc3545140cbbaf48d1bdb5a6
newdigate <nicnewdigate@gmail.com>
2025-01-26 14:39:39 +0000

    delta time sequencer

--- test/test_deltatimesequencer.cpp
+++ test/test_deltatimesequencer.cpp
@@ -1,37 +1,57 @@
 #include <boost/test/unit_test.hpp>
 #include "default_test_fixture.h"
 #include "DeltaTimeSequencer.h"
-#define ONE_SECOND 1000000;
+#define ONE_SECOND 1000000
 BOOST_AUTO_TEST_SUITE(basic_DeltaTimeSequencertest)
 
     BOOST_FIXTURE_TEST_CASE(can_sequence, DefaultTestFixture) {
 
         SD.setSDCardFolderPath("./output", true);
 
-        unsigned microseconds = 10 * ONE_SECOND;
-        unsigned microsPerTick = DeltaTimeSequencer::calculateMicrosPerTick(60.0, 1);
+        unsigned long microseconds = 10 * ONE_SECOND;
+        unsigned long microsPerTick = DeltaTimeSequencer::calculateMicrosPerTick(60.0, 1);
         DeltaTimeSequencer deltaTimeSequencer(microsPerTick, false);
         deltaTimeSequencer.start(microseconds);
-        Serial.printf("Start time: %d\n", microseconds);
+        unsigned long timeMicroseconds = deltaTimeSequencer.getMicroseconds(microseconds);
+        unsigned long timeInactivity = deltaTimeSequencer.getInactivityMicros(microseconds);
+        BOOST_CHECK_EQUAL(timeMicroseconds, 0);
+        BOOST_CHECK_EQUAL(timeInactivity, 0);
+        Serial.printf("Start: microseconds=%d\ttime=%d\tinactivity=%d\n", microseconds, timeMicroseconds, timeInactivity);
 
         microseconds += ONE_SECOND;
         auto delta = deltaTimeSequencer.get_delta(microseconds);
-        Serial.printf("  micros=%d delta=%d\n", microseconds, delta);
+        timeMicroseconds = deltaTimeSequencer.getMicroseconds(microseconds);
+        timeInactivity = deltaTimeSequencer.getInactivityMicros(microseconds);
+        Serial.printf("\t\tdelta=%d\tmicroseconds=%d\ttime=%d\tinactivity=%d\n\n", delta, microseconds, timeMicroseconds, timeInactivity);
+        BOOST_CHECK_EQUAL(timeMicroseconds, ONE_SECOND );
+        BOOST_CHECK_EQUAL(timeInactivity, 0);
         BOOST_CHECK_EQUAL(delta, 1);
 
         microseconds += ONE_SECOND;
         delta = deltaTimeSequencer.get_delta(microseconds);
-        Serial.printf("  micros=%d delta=%d\n", microseconds, delta);
+        timeMicroseconds = deltaTimeSequencer.getMicroseconds(microseconds);
+        timeInactivity = deltaTimeSequencer.getInactivityMicros(microseconds);
+        Serial.printf("\t\tdelta=%d\tmicroseconds=%d\ttime=%d\tinactivity=%d\n\n", delta, microseconds, timeMicroseconds, timeInactivity);
+        BOOST_CHECK_EQUAL(timeMicroseconds, 2 * ONE_SECOND );
+        BOOST_CHECK_EQUAL(timeInactivity, 0);
         BOOST_CHECK_EQUAL(delta, 1);
 
         microseconds += 2 * ONE_SECOND;
         delta = deltaTimeSequencer.get_delta(microseconds);
-        Serial.printf("  micros=%d delta=%d\n", microseconds, delta);
+        timeMicroseconds = deltaTimeSequencer.getMicroseconds(microseconds);
+        timeInactivity = deltaTimeSequencer.getInactivityMicros(microseconds);
+        Serial.printf("\t\tdelta=%d\tmicroseconds=%d\ttime=%d\tinactivity=%d\n\n", delta, microseconds, timeMicroseconds, timeInactivity);
+        BOOST_CHECK_EQUAL(timeMicroseconds, 4 * ONE_SECOND );
+        BOOST_CHECK_EQUAL(timeInactivity, 0);
         BOOST_CHECK_EQUAL(delta, 2);
 
         microseconds += 10 * ONE_SECOND;
         delta = deltaTimeSequencer.get_delta(microseconds);
-        Serial.printf("  micros=%d delta=%d\n", microseconds, delta);
+        timeMicroseconds = deltaTimeSequencer.getMicroseconds(microseconds);
+        timeInactivity = deltaTimeSequencer.getInactivityMicros(microseconds);
+        Serial.printf("\t\tdelta=%d\tmicroseconds=%d\ttime=%d\tinactivity=%d\n\n", delta, microseconds, timeMicroseconds, timeInactivity);
+        BOOST_CHECK_EQUAL(timeMicroseconds, 14 * ONE_SECOND );
+        BOOST_CHECK_EQUAL(timeInactivity, 0);
         BOOST_CHECK_EQUAL(delta, 10);
 
         microseconds += 3 * ONE_SECOND;
